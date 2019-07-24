8c52f03a9d0907a81786a47f4db36bdecdbed315
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-07-24 19:48:11 +0530

    Initial commit To do App with Reactjs

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import TodoItem from "./components/TodoItem"
+import todosData from "./components/todosData"
+
+import "./App.css"
+
+function App() {
+    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
+
+    return (
+        <div className="todo-list">
+            {todoItems}
+        </div>
+    )
+}
+
+export default App
