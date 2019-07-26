e42f28f8c2b6ca4214bd9ba0586e079219bd573e
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-07-26 22:38:40 +0530

    Added check-uncheck feature

--- src/App.js
+++ src/App.js
@@ -14,9 +14,17 @@ class App extends Component {
     }
 
     handleChange(id) {
-        // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
-        // Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
-        console.log("Changed", id)
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
     }
 
     render() {
