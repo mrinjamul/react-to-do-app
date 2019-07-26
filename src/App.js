8371026f0fcb9712fad0ff1a29b68717ea6a188e
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-07-26 20:07:26 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import todosData from "./components/todosData"
 
 import "./App.css"
 
-class App extends  Component {
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+    }
 
     render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
 
-    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
-
-    return (
-        <div className="todo-list">
-            {todoItems}
-        </div>
-    )
-  }
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )
+    }
 }
 
 export default App
